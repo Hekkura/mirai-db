ae835174328507433e3136cb428f6b1d46281e52
prog8melawai <prog8melawai@gmail.com>
2025-02-11 17:19:51 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import card from './components/card.vue';
+</script>
+
+<template>
+  <header class="text-lg font-fira p-5">
+    <h1 class=" text-gray-700 text-3xl text-center uppercase 
+                hover:text-gray-900 hover:scale-105 transition-all duration-500">
+      Mirai<Strong>DB</Strong>
+    </h1>
+
+	<form class="flex justify-center p-5 mx-auto max-w-[800px] w-full">
+		<input 	type="search" name="" id="" placeholder="Search for an anime..."
+				class="w-[100%] p-3 mix-blend-difference rounded-xl hover:outline-3 focus:outline-3 focus:shadow-2xs
+					outline-slate-400 shadow-lg bg-slate-200" 
+				required
+		>
+	</form>
+
+  </header>
+  <main class="p-7 flex flex-wrap gap-1">
+	<card/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<div class="p-5 bg-slate-500">
+        <a href="#" target="_blank">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBoPUzq_mhJLnhw2E74H9dEOUzjPt8v8iqkg&s" alt="Placeholder Title Image">
+            <h3 class="font-bold py-1 max-w-[50%]">Kamisato Ayaka</h3>
+        </a>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
