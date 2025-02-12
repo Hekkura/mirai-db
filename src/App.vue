0736ea9b39c8cb296e086a2d902f6d1f1d1c4ab5
prog8melawai <prog8melawai@gmail.com>
2025-02-12 16:45:35 +0700

    mvp

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,67 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
-import card from './components/card.vue';
+import Card from './components/Card.vue';
 
-import { ref } from 'vue';
 
 const searchQuery = ref("")
 const animeList = ref([])
-// const handleSearch = async () => { 
-// 						animeList.value = await fetch(``)
+const loading = ref(false)
+const error = ref('')
 
-// 					}
+async function handleSearch() {
+	loading.value = true
+	try{ 
+		const response = await fetch(`https://api.jikan.moe/v4/anime?q=${searchQuery.value}`)
+		if(!response.ok){
+			throw new Error (`HTTP Error : ${response.status}`)
+		}
+		const result = await response.json()
+		animeList.value = result.data
+		
+		// .then(res => res.json())
+		// 	.then(data => data.results);
+			// console.log(animeList.value)
+	} catch (err) { 
+		error.value = err instanceof Error ? err.message : String(err);
+		console.log(`Error : ${err}`)
+	} finally { 
+		loading.value = false
+	}
+}
+
+//DEBUG
+// watch(searchQuery, () => { 
+// 	console.log(`Search Query : ${searchQuery.value}`)
+
+// })
+
+// watch(handleSearch, () => {
+// 	console.log(`HandleSearch : ${handleSearch}`)
+// })
 
 </script>
 
 <template>
-  <header class="text-lg font-fira p-5">
+  <header class="text-lg font-fira p-5 overflow-hidden">
     <h1 class=" text-gray-700 text-3xl text-center uppercase 
                 hover:text-gray-900 hover:scale-105 transition-all duration-500">
-      Mirai<Strong>DB</Strong>
+      Mirai<b>DB</b>
     </h1>
 
-	<form class="flex justify-center p-5 mx-auto max-w-[800px] w-full">
+	<form class="flex justify-center p-5 mx-auto max-w-[800px] w-full" @submit.prevent="handleSearch">
 		<input 	type="search" name="" id="" placeholder="Search for an anime..."
 				class="w-[100%] p-3 mix-blend-difference rounded-xl hover:outline-3 focus:outline-3 focus:shadow-2xs
 					outline-slate-400 shadow-lg bg-slate-200" 
-				required
+				required v-model="searchQuery"
 		>
 	</form>
 
   </header>
-  <main class="p-7 flex flex-wrap gap-1">
-	<card/>
+  <main class="p-7 flex flex-wrap gap-1 justify-center">
+	  <Card v-for="anime in animeList" 
+	  			:anime="anime"
+	  />
   </main>
 </template>
 
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
-
+const props = defineProps<{
+    anime: any
+}>()
+console.log(`Image URL : ${props.anime.images.webp.image_url}`)
 </script>
 
 <template>
-	<div class="p-5">
+	<div class="p-5 max-w-[220px]">
         <a href="#" target="_blank">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBoPUzq_mhJLnhw2E74H9dEOUzjPt8v8iqkg&s" alt="Placeholder Title Image"
-                class="w-full max-h-[300px] object-cover rounded-lg shadow-md transition-all duration-500
+            <img :src="props.anime.images.webp.image_url" alt="Image Placeholder"
+                class="w-full max-h-[350px] max-w-[] object-cover rounded-lg shadow-md transition-all duration-500
                         hover:scale-105"
             >
-            <h3 class="font-bold py-1 max-w-[50%] transition-colors duratoin-500 hover:text-teal-700">Kamisato Ayaka</h3>
+            <h3 class="font-bold py-1 max-w-[80%] transition-colors duratoin-500 hover:text-teal-700">{{ props.anime.title }}</h3>
         </a>
 	</div>
 </template>
